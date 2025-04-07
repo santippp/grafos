e65f2d9709f753992f41b7cf70bd7f13100c4574
santippp <spriccofrakich@gmail.com>
2025-04-07 19:11:29 -0300

    terminando clase

---
+++ Grafos/Ejercicios/practica.py
@@ -0,0 +1,7 @@
+def mostrar_saludo():
+    print("hola que tal")
+
+def lee_grafo_stdin(grafo):
+    cant_vert = int(grafo[0])
+    vert = grafo[1 : cant_vert + 1]
+    arista = grafo [cant_vert + 1 : ]
---
+++ main.py
@@ -0,0 +1,3 @@
+import Grafos.Ejercicios.practica as p1
+
+p1.mostrar_saludo()
